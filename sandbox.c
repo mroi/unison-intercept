109cb3cc1e65d58d40aec4ed39e8f9117faf27b0
Michael Roitzsch <reactorcontrol@icloud.com>
2019-06-16 15:26:24 +0200

    sandbox all functions used by Unison

--- sandbox.c
+++ sandbox.c
@@ -19,9 +19,12 @@
 #include <assert.h>
 #include <dlfcn.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 
 #define ORIGINAL_SYMBOL(symbol, arguments) \
@@ -102,6 +105,66 @@ static enum sandbox_result sandbox_test(const char *path, enum sandbox_access ac
 
 /* MARK: - Sandboxed Functions */
 
+int access(const char *path, int mode)
+{
+	ORIGINAL_SYMBOL(access, (const char *path, int mode))
+	if (sandbox_test(path, (mode & W_OK) ? WRITE : READ) == PASS)
+		return original_access(path, mode);
+
+	errno = EACCES;
+	return -1;
+}
+
+int chdir(const char *path)
+{
+	ORIGINAL_SYMBOL(chdir, (const char *path))
+	enforce_sandbox(path, READ);
+	return original_chdir(path);
+}
+
+int chmod(const char *path, mode_t mode)
+{
+	ORIGINAL_SYMBOL(chmod, (const char *path, mode_t mode))
+	enforce_sandbox(path, WRITE);
+	return original_chmod(path, mode);
+}
+
+int chown(const char *path, uid_t owner, gid_t group)
+{
+	ORIGINAL_SYMBOL(chown, (const char *path, uid_t owner, gid_t group))
+	enforce_sandbox(path, WRITE);
+	return original_chown(path, owner, group);
+}
+
+int link(const char *target, const char *path)
+{
+	ORIGINAL_SYMBOL(link, (const char *target, const char *path))
+	enforce_sandbox(target, WRITE);  // otherwise privileges can be escalated via new path
+	enforce_sandbox(path, WRITE);
+	return original_link(target, path);
+}
+
+int lstat(const char *restrict path, struct stat *restrict stat)
+{
+	ORIGINAL_SYMBOL(lstat, (const char *restrict path, struct stat *restrict stat))
+	enforce_sandbox(path, READ);
+	return original_lstat(path, stat);
+}
+
+int mkdir(const char *path, mode_t mode)
+{
+	ORIGINAL_SYMBOL(mkdir, (const char *path, mode_t mode))
+	enforce_sandbox(path, WRITE);
+	return original_mkdir(path, mode);
+}
+
+int mkfifo(const char *path, mode_t mode)
+{
+	ORIGINAL_SYMBOL(mkfifo, (const char *path, mode_t mode))
+	enforce_sandbox(path, WRITE);
+	return original_mkfifo(path, mode);
+}
+
 int open(const char *path, int flags, ...)
 {
 	ORIGINAL_SYMBOL(open, (const char *path, int flags, ...))
@@ -122,6 +185,71 @@ int open(const char *path, int flags, ...)
 
 	va_end(arg);
 	return result;
+}
+
+DIR *opendir(const char *path)
+{
+	ORIGINAL_SYMBOL(opendir_int, (void))
+	DIR *(*original_opendir)(const char *) = (DIR *(*)(const char *))original_opendir_int;
+	enforce_sandbox(path, READ);
+	return original_opendir(path);
+}
+
+ssize_t readlink(const char *restrict path, char *restrict buf, size_t size)
+{
+	ORIGINAL_SYMBOL(readlink, (const char *restrict path, char *restrict buf, size_t size))
+	enforce_sandbox(path, READ);
+	return original_readlink(path, buf, size);
+}
+
+int rename(const char *old, const char *new)
+{
+	ORIGINAL_SYMBOL(rename, (const char *old, const char *new))
+	enforce_sandbox(old, WRITE);
+	enforce_sandbox(new, WRITE);
+	return original_rename(old, new);
+}
+
+int rmdir(const char *path)
+{
+	ORIGINAL_SYMBOL(rmdir, (const char *path))
+	enforce_sandbox(path, WRITE);
+	return original_rmdir(path);
+}
+
+int stat(const char *restrict path, struct stat *restrict stat)
+{
+	ORIGINAL_SYMBOL(stat, (const char *restrict path, struct stat *restrict stat))
+	enforce_sandbox(path, READ);
+	return original_stat(path, stat);
+}
+
+int symlink(const char *target, const char *path)
+{
+	ORIGINAL_SYMBOL(symlink, (const char *target, const char *path))
+	enforce_sandbox(path, WRITE);
+	return original_symlink(target, path);
+}
+
+int truncate(const char *path, off_t length)
+{
+	ORIGINAL_SYMBOL(truncate, (const char *path, off_t length))
+	enforce_sandbox(path, WRITE);
+	return original_truncate(path, length);
+}
+
+int unlink(const char *path)
+{
+	ORIGINAL_SYMBOL(unlink, (const char *path))
+	enforce_sandbox(path, WRITE);
+	return original_unlink(path);
+}
+
+int utimes(const char *path, const struct timeval times[2])
+{
+	ORIGINAL_SYMBOL(utimes, (const char *path, const struct timeval times[2]))
+	enforce_sandbox(path, WRITE);
+	return original_utimes(path, times);
 }
 
 
@@ -165,7 +293,7 @@ BLOCK(fchmodat, int, (unused int fd, unused const char *path, unused mode_t mode
 BLOCK(fchownat, int, (unused int fd, unused const char *path, unused uid_t owner, unused gid_t group, unused int flags))
 BLOCK(fstatat, int, (unused int fd, unused const char *path, unused struct stat *stat, unused int flags))
 BLOCK(futimesat, int, (unused int fd, unused const char *path, unused const struct timeval times[2]))
-BLOCK(linkat, int, (unused int fd1, unused const char *path1, unused int fd2, unused const char *path2, unused int flags))
+BLOCK(linkat, int, (unused int fd1, unused const char *target, unused int fd2, unused const char *path, unused int flags))
 BLOCK(mkdirat, int, (unused int fd, unused const char *path, unused mode_t mode))
 BLOCK(mknodat, int, (unused int fd, unused const char *path, unused mode_t mode, unused dev_t dev))
 BLOCK(openat, int, (unused int fd, unused const char *path, unused int flags, ...))
